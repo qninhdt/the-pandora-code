614f64440893022c6e7d2f4218212d7cb6360c43
qninhdt <qndt123@gmail.com>
2026-08-23 03:24:23 +0700

    chore(repo): remove obsolete scripts and tag dumps

--- scripts/test-yaml.ts
+++
@@ -1,8 +0,0 @@
-import { readFileSync, writeFileSync } from "fs";
-import yaml from "js-yaml";
-
-const content = readFileSync("content/chapters/where-is-pandora/meta.yaml", "utf8");
-const data = yaml.load(content);
-
-const output = yaml.dump(data, { lineWidth: -1 });
-console.log(output);
